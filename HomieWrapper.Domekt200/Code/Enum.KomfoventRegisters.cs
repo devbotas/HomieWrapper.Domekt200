61c26d32005fb5567918ea5cd13fe2a45fef8619
devbotas <simon@devbot.lt>
2021-04-27 11:30:14 +0300

    Initial commit.

---
+++ HomieWrapper.Domekt200/Code/Enum.KomfoventRegisters.cs
@@ -0,0 +1,27 @@
+﻿namespace HomieWrapper.Domekt200 {
+    enum KomfoventRegisters : ushort {
+        // These are all -1'ed, because that's how Modbus works...
+
+        // General section.
+        StartStop = 10000,
+        Season,
+        AlarmStatusWarnings,
+        C4Configuration,
+        HourAndMinute,
+        DayOfWeek,
+        MonthAndDay,
+        Year,
+        AlarmStatusStopFlags,
+        AlarmStatusStopCode,
+        ModbusAddress,
+        ExternalHeaterControlSignalType,
+
+        // Ventilation section.
+        VentilationLevelManual = 10100,
+        VentilationLevelCurrent,
+        ModeAutoManual,
+
+        // Temperatures section.
+        SupplyAirTemperature = 10300
+    }
+}
---
+++ HomieWrapper.Domekt200/Code/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using DevBot9.Protocols.Homie;
+using NLog;
+using NLog.Config;
+
+namespace HomieWrapper.Domekt200 {
+    class Program {
+        private static Domekt200 _recuperator = new Domekt200();
+
+        public static Logger Log = LogManager.GetLogger("ShedMonitor.Main");
+        static void Main(string[] args) {
+            // Load environment variables.
+            var brokerIp = Environment.GetEnvironmentVariable("MQTT_BROKER_IP");
+            if (string.IsNullOrEmpty(brokerIp)) {
+                Console.WriteLine("Evironment variable \"MQTT_BROKER_IP\" is not provided. Using 127.0.0.1.");
+                brokerIp = "127.0.0.1";
+            }
+            var domektIp = Environment.GetEnvironmentVariable("DOMEKT_IP");
+            if (string.IsNullOrEmpty(domektIp)) {
+                Console.WriteLine("Evironment variable \"DOMEKT_IP\" is not provided. Using 127.0.0.1.");
+                domektIp = "127.0.0.1";
+            }
+
+            var logsFolder = Directory.GetCurrentDirectory(); // Path.Combine(LogsFolder);
+
+            // NLog doesn't like backslashes.
+            logsFolder = logsFolder.Replace("\\", "/");
+
+            // Finalizing configuration.
+            LogManager.Configuration = new XmlLoggingConfiguration(new XmlTextReader(new MemoryStream(Encoding.UTF8.GetBytes(Properties.Resources.NLogConfig.Replace("!LogsFolderTag!", logsFolder)))), "NLogConfig.xml");
+
+            Log.Info("Application started.");
+            DeviceFactory.Initialize("homie");
+            _recuperator.Initialize(brokerIp, domektIp);
+
+            Console.ReadLine();
+        }
+    }
+}
